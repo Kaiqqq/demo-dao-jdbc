39bfa3b68dce5d74f6cf375b03f652cf71f2c583
Kaiqqq <kai-quelopes@hotmail.com>
2023-06-26 17:48:46 -0300

    SellerDao class - update method implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -30,5 +30,18 @@ public class Program {
             System.out.println(obj);
         }
 
+        /*
+        System.out.println("\n== Test 4: seller insert Method ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 3000.0, dep);
+        sellerDao.insert(newSeller);
+
+        System.out.println("Data inserted! Seller generated ID: " + newSeller.getId());
+         */
+
+        System.out.println("\n== Test 5: seller update method ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Wayne");
+        sellerDao.update(seller);
+        System.out.println("Seller updated!");
     }
 }
