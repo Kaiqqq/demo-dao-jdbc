5e9d5c13fddff1f91e47ffe8668ed3443c0624de
Kaiqqq <kai-quelopes@hotmail.com>
2023-06-26 18:04:45 -0300

    Seller delete method implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,10 +7,12 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("== Test 1: seller findById Method ===");
@@ -30,18 +32,25 @@ public class Program {
             System.out.println(obj);
         }
 
-        /*
-        System.out.println("\n== Test 4: seller insert Method ===");
+
+        /*System.out.println("\n== Test 4: seller insert Method ===");
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 3000.0, dep);
         sellerDao.insert(newSeller);
 
-        System.out.println("Data inserted! Seller generated ID: " + newSeller.getId());
-         */
+        System.out.println("Data inserted! Seller generated ID: " + newSeller.getId());*/
+
 
         System.out.println("\n== Test 5: seller update method ===");
         seller = sellerDao.findById(1);
         seller.setName("Martha Wayne");
         sellerDao.update(seller);
         System.out.println("Seller updated!");
+
+        System.out.println("\n== Test 6: seller delete method ===");
+        System.out.print("Write seller id to be deleted: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+
+        sc.close();
     }
 }
